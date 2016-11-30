01ac7c4c6f7f8a5f802fc4969eb29c7ecc28266b
dylanmitchell1007 <armadummy@yahoo.com>
2016-11-30 17:21:10 -0600

    nothing

---
+++ GTest/Header.h
@@ -0,0 +1,155 @@
+#include <iostream>
+#include <Windows.h>
+
+
+//Create a function that takes in 3 numbers and returns the sum of those numbers multiplied by 5
+//(This function should work for both integer and floating point values).
+
+//1.
+
+
+
+float Desk(int a, int b, int c)
+{
+	
+	return ( a +  b +  c) * 5;
+
+
+
+
+
+
+}
+
+
+
+
+//Write a function that takes two parameters a pointer to an array and the number of elements then populate the array with all even numbers.
+
+//2.
+//float bus(char *a, char *b);
+//{
+	//float bus[6];
+
+
+//}
+
+
+
+
+
+
+
+//Create a Function that Takes in a Character ARRAY and REVERSES the string stored inside of it.
+//3.
+/*char ReverseRay[10];*/
+//{
+
+
+//}
+
+
+
+
+
+
+//Create a CAT CLASS with a private CHAR Variable called m_name. Then create a public constructor that has no arguements. You will need one more function of return type void called NameMe,
+//This takes in a character as an arguement and needs to assign this character to the value of m_name. Once you have your class created, Create an array of type Cat of size 26. Then populate the names of
+//Then populate the names of these cats each with a different letter of the alphabet starting with Capital A and every other cats name should be a lowercase letter (A,b,C,d,E,f,G...).
+//YOU CANNOT USE A FOR LOOP AND YOU CANNOT PRINT TO THE CONSOLE!!
+//4.
+
+
+
+
+/*class Cat {};
+{
+Cat[26];
+{
+
+}
+
+public:
+Meow();
+void *NameMe;
+
+
+private:
+char *m_name;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Create a function that takes in two arguments, One that points to an array of integers and one for the size of the array.
+//This function will return the value in the array that appears the most times
+//5.
+
+
+
+
+
+
+
+
+//Create a function that swaps the value of two integers without the use of a temporary variable.
+//6.
+//float Swap(float a, float b);
+//{
+	//int a = 5;
+	//int b = 2;
+
+
+
+//}
+
+
+
+
+
+
+//Create an array of strings then sort each string in alphabetical order.
+//7.
+//string Array[2];
+//{
+
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Windows.h>
+#include "GTest\Header.h"
+using namespace std;
+
+
+int main()
+{
+	//char ReverseArray[];
+	float Desk(int, int, int);
+	float Swap(float a, float b);
+	
+	
+
+	
+	system("pause");
+ }
